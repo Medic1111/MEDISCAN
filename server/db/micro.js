cdb1141ea4b3d02ff905246d964342d7b9ca9ab1
Aryse Tansy <medictansy@gmail.com>
2023-01-20 13:02:17 -0500

    Feature: Send Email Util, SEEDDB MicroService

---
+++ server/db/micro.js
@@ -0,0 +1,41 @@
+const User = require("../models/user");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = async () => {
+  mongoose.set("strictQuery", true);
+  await mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log("DB READY TO SEED"))
+    .catch((err) => console.log(err));
+};
+
+const mockClient = {
+  name: "Mock Client",
+  email: "mockclient@mock.com",
+  password: "mockclient",
+  allergies: ["pollen"],
+  medical_history: ["CHF", "Emphysema", "Asthma"],
+  medications: ["inhaler", "Oxygen NC@6lpm"],
+};
+
+const seedDB = async () => {
+  await connection();
+  await User.create(mockClient)
+    .then((newUser) => console.log("SEEDED: ", newUser))
+    .catch((err) => console.log(err));
+
+  process.exit();
+};
+
+const clearDB = async () => {
+  await connection();
+  await User.deleteMany()
+    .then(() => console.log("CLEARED DB"))
+    .catch((err) => console.log(err));
+
+  process.exit();
+};
+
+if (process.argv[2] === "-seed") return seedDB();
+if (process.argv[2] === "-clear") return clearDB();
