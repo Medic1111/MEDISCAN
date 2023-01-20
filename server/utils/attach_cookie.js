f77f23dba4a9b213fe6635be0ac08cb2a9704a6b
Aryse Tansy <medictansy@gmail.com>
2023-01-20 12:36:41 -0500

    Utils: Cookie, Token, HandleAsync

---
+++ server/utils/attach_cookie.js
@@ -0,0 +1,14 @@
+const signToken = require("./sign_token");
+
+const attachCookie = (req, res, next) => {
+  let token = signToken(req.body.username);
+  res.cookie("jwt", token, {
+    maxAge: 3600000,
+    secure: true,
+    httpOnly: true,
+  });
+
+  next();
+};
+
+module.exports = attachCookie;
---
+++ server/utils/handle_async.js
@@ -0,0 +1,7 @@
+const handleAsync = (fn) => {
+  return (req, res, next) => {
+    return fn(req, res, next).catch(next);
+  };
+};
+
+module.exports = handleAsync;
