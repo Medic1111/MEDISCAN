2ddde73f566906848e15dd4f78f60ba30619291c
ri-tae-ka <ritzthareja@gmail.com>
2023-01-21 00:40:34 +0530

    user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,51 @@
-// PLEASE INSERT SCHEMA HERE
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please enter your name"],
+    minLength: [4, "Name should be atleast 4 characters"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: [true, "Email is already in use, please use another email"],
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [8, "Name should be atleast 8 characters"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    select: false,
+  },
+  allergies: [{ type: String }],
+  medical_history: [{ type: String }],
+  medications: [{ type: String }],
+  joinedDate: {
+    type: Date,
+    default: new Date().toISOString(),
+    select: false,
+  },
+  resetPasswordToken: String,
+  resePasswordExpire: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.method.getJWTToken = function () {
+  return jwt.sign({ id: this.id }, process.env.TOKEN_SECRET, {
+    expiresIn: process.env.TOKEN_EXPIRE,
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
